ff7c9b884cb8ec8e6e4fcb13b0b1f025411df9ce
ShivangM <shivangmishra0824@gmail.com>
2022-07-05 10:04:02 +0530

    DNF

---
+++ DNFSort.cpp
@@ -0,0 +1,90 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h> 
+#include <complex>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+
+void swap(v32 &arr, int i, int j){
+    int tmp =  arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+}
+
+void dnfSort(v32 &arr){
+    int n = arr.size(); int low = 0; int high = n-1; int mid = 0;
+    while(mid <= high){
+        if(arr[mid] == 0) {swap(arr, low, mid); low++; mid++;}
+        else if(arr[mid] == 1){ mid++; }
+        else {swap(arr, mid, high); high--;}
+    }
+}
+
+void solve(){
+    v32 arr = {1,0,2,1,0,1,2,1,2};
+    dnfSort(arr); forn(i, arr.size()) cout<<arr[i]<<" ";
+}
+
+int main()
+{
+ fast_cin();
+ #ifndef ONLINE_JUDGE
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+#endif
+
+ ll t;
+ cin >> t;
+ for(int it=1;it<=t;it++) {
+    solve();
+ }
+ return 0;
+}
