1b1afd4851e6d1260ddeda28f358a4e75c9b8308
ShivangM <shivangmishra0824@gmail.com>
2022-07-13 10:14:11 +0530

    infix-to-postfix

--- StackQuestions.cpp
+++ StackQuestions.cpp
@@ -166,7 +166,44 @@ int postfixEvaluation(string s){
     return st.top();
 }
  
+int precidence(char c){
+    if(c == '^') return 3;
+    else if(c == '*' || c == '/') return 2;
+    else if(c == '-' || c == '+') return 1;
+    else return -2;
+}
 
+string infixToPostfix(string s){
+    stack<char> st;
+    string res = "";
+
+    forn(i, s.length()){
+        if((s[i] >= 'a' && s[i] <= 'z') || (s[i]>= 'A' && s[i] <= 'Z')){
+            res += s[i];
+        }
+        else if(s[i] = '('){
+            st.push(s[i]);
+        }
+        else if(s[i] = ')'){
+            while(!st.empty() && st.top() != '('){
+                res += st.top(); st.pop();
+            }
+            if(!st.empty()) st.pop();
+        }
+        else{
+            while (!st.empty() && (precidence(st.top()) > precidence(s[i]))){
+                res += st.top(); st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+
+    while (!st.empty())
+    {
+        res += st.top(); st.pop();
+    }
+    return res;
+}
 
 void solve(){
     // string s = "Hey how are you doing?";
@@ -179,8 +216,9 @@ void solve(){
     // reverseStack(st);
     // printStack(st, st.size());
 
-    cout<<prefixEvaluation("-+7*45+20")<<endl;
-    cout<<postfixEvaluation("46+2/5*7+")<<endl;
+    // cout<<prefixEvaluation("-+7*45+20")<<endl;
+    // cout<<postfixEvaluation("46+2/5*7+")<<endl;
+    cout<<infixToPostfix("(a-b/c)*(a/k-l)")<<endl;
 }
 
 int main()
